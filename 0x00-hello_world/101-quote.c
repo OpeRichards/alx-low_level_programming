c748def8bd30520aa2374b853d35ce1eadbd3098
Opeyemi Richard <opeyemirichard@gmail.com>
2023-09-07 22:05:35 -0700

    C program for task 8. UNIX ...

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,9 @@
  */
 int main(void)
 {
-		char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-		fprintf(stderr, "%s", S);
+
+	fprintf(stderr, "%s", S);
 	return (1);
 }

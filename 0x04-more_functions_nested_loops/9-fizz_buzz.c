27aec2e39a72cbc970bd625db2978a597ace0e44
Opeyemi Richard <opeyemirichard@gmail.com>
2023-10-04 10:28:02 -0700

    Printnumbers 1 to 100 with 'Fizz-Buzz test'

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,24 @@ int main(void)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("fizzbuzz ");
+			printf("fizzbuzz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("buzz ");
+			printf("buzz");
 		}
 		else if (n % 3 == 0)
 		{
-			printf("fizz ");
+			printf("fizz");
 		}
 		else
 		{
-			printf("%d ", n);
+			printf("%d", n);
+		}
+
+		if (n < 100)
+		{
+			printf(" ");
 		}
 	}
 

7e7bf816615ba8c12a0f9496d311d4d8208b6327
Opeyemi Richard <opeyemirichard@gmail.com>
2023-10-03 22:01:03 -0700

    Prints digits 0 to 14 in 10 rows

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - Prints digits 0 to 14 in 10 different times
+ * 
+ */
+void more_numbers(void)
+{
+	int rows;
+	int digits;
+
+	for (rows = 0; rows <= 9; rows++)
+	{
+		for (digits = 0; digits <= 14; digits++)
+		{
+			if (digits > 9)
+			{
+				_putchar((digits / 10) + '0');
+			}
+			_putchar((digits % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}

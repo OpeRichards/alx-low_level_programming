e468ecd723e0a30dcb4465d7161360896967a89a
Opeyemi Richard <opeyemirichard@gmail.com>
2023-10-04 10:09:15 -0700

    Printnumbers 1 to 100 with 'Fizz-Buzz test'

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("fizzbuzz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("buzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("fizz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+
+	_putchar('\n');
+
+	return (0);
+}

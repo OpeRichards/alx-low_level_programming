84fa101994c0bdc2e4a6e27d594ef3886416ea89
Opeyemi Richard <opeyemirichard@gmail.com>
2023-10-13 05:58:43 -0700

    Returns the index of an eleement in an array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,3 +10,17 @@
  * Return: -1 if NULL or size <= 0 else int_index
  */
 int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]))
+			return (index);
+	}
+
+	return (-1);
+}

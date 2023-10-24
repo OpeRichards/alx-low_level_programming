b7790a3f5cd0cf4722819f11a5d3b54ec3214f31
Opeyemi Richard <opeyemirichard@gmail.com>
2023-10-24 12:49:09 -0700

    Deletes the head node of 'listint_t' linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,7 @@
 
 /**
  * print_listint - Prints all elements of a 'listint-t' list
- * *@h: Pointer to the first element
+ * @h: Pointer to the first element
  * Return: Number of nodes
  */
 size_t print_listint(const listint_t *h)
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Deletes the head node of 'listint_t' linked list
+ * @head: Head of the linked list
+ * Return: Head node's data(n), 0 if linked list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int nodes;
+
+	if (!head || !*head)
+		return (0);
+
+	nodes = (*head)->n;
+	tmp = (*head)->next;
+	free(*head);
+	*head = tmp;
+	
+	return (nodes);
+}
